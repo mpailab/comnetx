f921bad457e2ebe5b15af49090bf74ff6d60da31
alkonoval <alexandr.konoval@gmail.com>
2025-08-08 19:28:48 +0300

    Add script for install necessary packages

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ComNetX
 Community Detection Using Neural Networks
+
+# Quick start
+
+Create new docker container:
+```shell
+$  ./scripts/create_docker_container.sh -v -n <my_container_name>
+``` 
+
+Install the necessary packages inside the created container <my_container_name>:
+```shell
+root@user_<my_container_name>$ bash ./scripts/install_packages
+```
---
+++ scripts/install_packages.sh
@@ -0,0 +1,27 @@
+# install python3.12
+# add-apt-repository ppa:deadsnakes/ppa
+# apt-get update
+# apt-get install python3.12
+# apt install python3.12-full
+# apt install python3-pip
+# python3.12 -m ensurepip --upgrade
+# python3.12 -m pip install --upgrade pip
+
+# install python3-graph-tool
+# apt install wget
+# wget https://downloads.skewed.de/skewed-keyring/skewed-keyring_1.1_all_$(lsb_release -s -c).deb
+# dpkg -i skewed-keyring_1.1_all_$(lsb_release -s -c).deb
+# echo "deb [signed-by=/usr/share/keyrings/skewed-keyring.gpg] https://downloads.skewed.de/apt $(lsb_release -s -c) main" > /etc/apt/sources.list.d/skewed.list
+# apt-get update
+# apt-get install python3-graph-tool
+
+# new
+pip install pybind11 # for sdp-clustering !!!
+pip install sdp-clustering
+pip install -r baselines/PRGPT/requirements.txt
+# torch.__version__  вместо 2.1.0+cu118
+pip install torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-2.1.0+cu118.html
+pip install ogb
+pip install matplotlib
+pip install pytest
+pip install tensorflow
