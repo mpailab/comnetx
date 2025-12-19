5f750aead00eab16e68527d7a1f454f1ced8054c
DrobyshevAS <drobyshev.sanya@yandex.ru>
2025-12-20 02:58:50 +0300

    update container scripts

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -44,8 +44,8 @@ pip install --no-cache-dir -r "$REQUIREMENTS_PATH"
 pip install --no-cache-dir \
     torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric \
     -f https://data.pyg.org/whl/torch-2.1.0+cu118.html \
-    ogb matplotlib tensorflow==2.14.0 pytest debugpy scikit-learn-intelex
+    ogb matplotlib tensorflow==2.14.0 pytest debugpy scikit-learn-intelex 
 
-pip install --no-cache-dir leidenalg networkit
+pip install --no-cache-dir leidenalg networkx==2.8.7 gudhi pot seaborn umap eagerpy
 
 echo "[INFO] Done!"
