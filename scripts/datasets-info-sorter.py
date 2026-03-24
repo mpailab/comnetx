10d1f99efb5cb8ce4783280cb7f7be59d83f2232
dev:alkonoval <alexandr.konoval@gmail.com>
2026-03-24 13:19:10 +0300

    restore removed files, hotfix in paths

--- scripts/datasets-info-sorter.py
+++ scripts/datasets-info-sorter.py
@@ -1,20 +1,26 @@
 import os.path
 import sys
 import json
+from pathlib import Path
 
 PROJECT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(os.path.join(PROJECT_PATH, "src"))
 from datasets import INFO, Dataset
 
-for fmt in Dataset.FILE_BASED_FORMATS:
-    path = INFO / f"{fmt}.json"
+# formats = Dataset.FILE_BASED_FORMATS
+# info = INFO
+formats = ["dsbm", "s2cag"]
+info = Path("datasets-info")
+
+for fmt in formats:
+    path = info / f"{fmt}.json"
     with open(path) as _:
         data = json.load(_)
 
     by_nodes = dict(sorted(data.items(), key=lambda item: item[1]['n']))
     by_edges = dict(sorted(data.items(), key=lambda item: item[1]['m']))
 
-    with open(INFO / "nodes-sorted" / f"{fmt}.json", 'w', encoding='utf-8') as _:
+    with open(info / "nodes-sorted" / f"{fmt}.json", 'w', encoding='utf-8') as _:
         json.dump(by_nodes, _, indent=4, ensure_ascii=False)
-    with open(INFO / "edges-sorted" / f"{fmt}.json", 'w', encoding='utf-8') as _:
+    with open(info / "edges-sorted" / f"{fmt}.json", 'w', encoding='utf-8') as _:
         json.dump(by_edges, _, indent=4, ensure_ascii=False)
