2945929061cf0b7b777bb56a8837d07e1d44f763
dev:alkonoval <alexandr.konoval@gmail.com>
2026-03-26 21:05:14 +0300

    fix paths in create_from_image.sh

--- scripts/docker/create_from_image.sh
+++ scripts/docker/create_from_image.sh
@@ -7,6 +7,13 @@ REUSE=0
 SHM_SIZE="2g"
 GPUS="all"
 
+SCRIPT_DIR=$(cd "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+PROJECT_USER=$(basename "$(dirname "$(dirname "$(dirname "$SCRIPT_DIR")")")")
+PROJECT_DIR="/home/dev/users/$PROJECT_USER/comnetx"
+CONFIG_DIR="$PROJECT_DIR/datasets-info"
+
+SERVER=$(hostname)
+
 function usage {
     echo "usage: $0 [-rmh] -n NAME [-m SHM_SIZE]"
     echo "  -n   Container's name"
@@ -83,29 +90,57 @@ if [ $REUSE -eq 1 ]; then
     $DOCKER rm $NAME || true
 fi 
 
+case "$SERVER" in
+    "astra")
+        SRC_PATHS="$CONFIG_DIR/paths_astra.json"
+        ;;
+    "cn69")
+        SRC_PATHS="$CONFIG_DIR/paths_cn69.json"
+        ;;
+    *)
+        echo "[ERROR] Unknown host: $SERVER"
+        exit 1
+        ;;
+esac
+
+if [ ! -f "$SRC_PATHS" ]; then
+    echo "[ERROR] Source paths file not found: $SRC_PATHS"
+    exit 1
+fi
+
+
+echo "SCRIPT_DIR: $SCRIPT_DIR"
+echo "PROJECT_USER: $PROJECT_USER"
+echo "CONFIG_DIR: $CONFIG_DIR"
+echo "SERVER: $SERVER"
+echo "SRC_PATHS: $SRC_PATHS"
+
 printf "  create $NAME as "
 $DOCKER create --gpus "device=$GPUS" -it --shm-size=$SHM_SIZE \
     -e TERM=xterm-256color \
     --entrypoint /bin/bash \
-    -w / \
-    -v /home/$USER:/home/$USER \
+    -w /home/dev/users/$PROJECT_USER/comnetx \
+    -v /home/$USER:/home/$PROJECT_USER \
     -v /home/$USER/.bashrc:/root/.bashrc --name $NAME -h $NAME $IMAGE
 
 printf "  start "
 $DOCKER start $NAME
 
+$DOCKER exec "$NAME" mkdir -p /home/dev/users/$PROJECT_USER/comnetx/datasets-info
+$DOCKER cp "$SRC_PATHS" "$NAME:/home/dev/users/$PROJECT_USER/comnetx/datasets-info/paths.json"
+
 $DOCKER exec -it $NAME /bin/bash
 
-read -p "Сохранить изменения в образ $IMAGE? (y/N): " SAVE
+read -p "Save changes to image $IMAGE? (y/N): " SAVE
 if [[ "$SAVE" =~ ^[Yy]$ ]]; then
-    echo "[INFO] Делаю commit..."
+    echo "[INFO] Commmiting changes..."
     $DOCKER commit $NAME $IMAGE
 
-    read -p "Отправить обновлённый образ в Docker Hub? (y/N): " PUSH
+    read -p "Push updated image to Docker Hub? (y/N): " PUSH
     if [[ "$PUSH" =~ ^[Yy]$ ]]; then
-        echo "[INFO] Пушу образ на Docker Hub..."
+        echo "[INFO] Pushing image to Docker Hub..."
         $DOCKER push $IMAGE
     fi
 fi
 
-echo "[INFO] Готово."
+echo "[INFO] Done."
