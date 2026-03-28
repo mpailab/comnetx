cd8d05f4b45dcbd1721bdf0f11ff4d0aabb24a23
dev:alkonoval <alexandr.konoval@gmail.com>
2026-03-28 11:48:42 +0300

    update container creation scripts + configs for test launches

--- scripts/docker/create_from_image.sh
+++ scripts/docker/create_from_image.sh
@@ -7,12 +7,14 @@ REUSE=0
 SHM_SIZE="2g"
 GPUS="all"
 
+
 SCRIPT_DIR=$(cd "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 PROJECT_USER=$(basename "$(dirname "$(dirname "$(dirname "$SCRIPT_DIR")")")")
 PROJECT_DIR="/home/dev/users/$PROJECT_USER/comnetx"
-CONFIG_DIR="$PROJECT_DIR/datasets-info"
+
 
 SERVER=$(hostname)
+
 
 function usage {
     echo "usage: $0 [-rmh] -n NAME [-m SHM_SIZE]"
@@ -24,13 +26,16 @@ function usage {
     exit 1
 }
 
+
 [ $# -eq 0 ] && usage
+
 
 PARSED_ARGUMENTS=$(getopt -n $0 -o n:m:rhg: -- "$@")
 VALID_ARGUMENTS=$?
 if [ "$VALID_ARGUMENTS" != "0" ]; then
     usage
 fi
+
 
 eval set -- "$PARSED_ARGUMENTS"
 while :
@@ -70,10 +75,12 @@ do
     esac
 done
 
+
 if [ $# -ne 0 ]; then
     echo "Unexpected arguments: $@ - this should not happen."
     usage
 fi
+
 
 if $DOCKER ps -a --format '{{.Names}}' | grep -q "^$NAME$"; then
     if [ $REUSE -eq 0 ]; then
@@ -81,6 +88,7 @@ if $DOCKER ps -a --format '{{.Names}}' | grep -q "^$NAME$"; then
         exit 1
     fi
 fi
+
 
 echo "$DOCKER"
 if [ $REUSE -eq 1 ]; then
@@ -90,51 +98,36 @@ if [ $REUSE -eq 1 ]; then
     $DOCKER rm $NAME || true
 fi 
 
-case "$SERVER" in
-    "astra")
-        SRC_PATHS="$CONFIG_DIR/paths_astra.json"
-        ;;
-    "cn69")
-        SRC_PATHS="$CONFIG_DIR/paths_cn69.json"
-        ;;
-    *)
-        echo "[ERROR] Unknown host: $SERVER"
-        exit 1
-        ;;
-esac
 
-if [ ! -f "$SRC_PATHS" ]; then
-    echo "[ERROR] Source paths file not found: $SRC_PATHS"
-    exit 1
-fi
+# Убрал блок выбора пути в зависимости от сервера (astra/cn69)
 
 
 echo "SCRIPT_DIR: $SCRIPT_DIR"
 echo "PROJECT_USER: $PROJECT_USER"
-echo "CONFIG_DIR: $CONFIG_DIR"
+echo "PROJECT_DIR: $PROJECT_DIR"
 echo "SERVER: $SERVER"
-echo "SRC_PATHS: $SRC_PATHS"
+
 
 printf "  create $NAME as "
 $DOCKER create --gpus "device=$GPUS" -it --shm-size=$SHM_SIZE \
     -e TERM=xterm-256color \
     --entrypoint /bin/bash \
     -w /home/dev/users/$PROJECT_USER/comnetx \
-    -v /home/$USER:/home/$PROJECT_USER \
+    -v /home/$USER:/home/$USER \
     -v /home/$USER/.bashrc:/root/.bashrc --name $NAME -h $NAME $IMAGE
+
 
 printf "  start "
 $DOCKER start $NAME
 
-$DOCKER exec "$NAME" mkdir -p /home/dev/users/$PROJECT_USER/comnetx/datasets-info
-$DOCKER cp "$SRC_PATHS" "$NAME:/home/dev/users/$PROJECT_USER/comnetx/datasets-info/paths.json"
-
 $DOCKER exec -it $NAME /bin/bash
+
 
 read -p "Save changes to image $IMAGE? (y/N): " SAVE
 if [[ "$SAVE" =~ ^[Yy]$ ]]; then
     echo "[INFO] Commmiting changes..."
     $DOCKER commit $NAME $IMAGE
+
 
     read -p "Push updated image to Docker Hub? (y/N): " PUSH
     if [[ "$PUSH" =~ ^[Yy]$ ]]; then
@@ -142,5 +135,6 @@ if [[ "$SAVE" =~ ^[Yy]$ ]]; then
         $DOCKER push $IMAGE
     fi
 fi
+
 
 echo "[INFO] Done."
